aee1735f48c7fe65a81bfcfc72fd02399ba87280
Seung-zedd <101092038+Seung-zedd@users.noreply.github.com>
2023-08-28 15:16:33 +0900

    Update site.config.js
    - 노션 아바타 절대경로 어떻게 적용하는지 파악 후 수정 필요

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "seung-zedd",
-    image: "/notion-avatar-1693197127066.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "Computer-Engineering Student(Undergraduate)",
     bio: "You never fail until you stop trying",
     email: "csz0209@gmail.com",
@@ -23,8 +23,8 @@ const CONFIG = {
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
+  link: "https://seung-zedd.vercel.app/",
+  since: 2023, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
